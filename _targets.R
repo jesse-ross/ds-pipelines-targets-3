caa4454678c21142191481d00f3ccaa45c42c2ed
Jesse Ross <jross@usgs.gov>
2021-09-22 11:36:32 -0700

    tally site data

--- _targets.R
+++ _targets.R
@@ -1,13 +1,20 @@
 library(targets)
 library(tarchetypes)
 library(tibble)
+suppressPackageStartupMessages(library(dplyr))
 
 options(tidyverse.quiet = TRUE)
-tar_option_set(packages = c("tidyverse", "dataRetrieval", "urbnmapr", "rnaturalearth", "cowplot"))
+tar_option_set(packages = c("cowplot",
+                            "dataRetrieval",
+                            "lubridate",
+                            "rnaturalearth",
+                            "tidyverse",
+                            "urbnmapr"))
 
 # Load functions needed by targets below
 source("1_fetch/src/find_oldest_sites.R")
 source("1_fetch/src/get_site_data.R")
+source("2_process/src/tally_site_obs.R")
 source("3_visualize/src/map_sites.R")
 
 # Configuration
@@ -21,13 +28,15 @@ list(
 
   # PULL SITE DATA HERE
   tar_map(
-    values = tibble(state_abb = states),
+    values = tibble(state_abb = states) %>%
+      mutate(state_plot_files = sprintf("3_visualize/out/timeseries_%s.png",
+                                        state_abb)),
     tar_target(nwis_inventory,
                filter(oldest_active_sites, state_cd == state_abb) ),
     tar_target(nwis_data, get_site_data(site_info = nwis_inventory,
                                         state = state_abb,
-                                        parameter = parameter))
-    # Insert step for tallying data here
+                                        parameter = parameter)),
+    tar_target(site_tallies, tally_site_obs(nwis_data))
     # Insert step for plotting data here
   ),
 
