64930faf4b73dad393129012b36d783bff4bd256
Jesse Ross <jross@usgs.gov>
2021-09-22 14:27:08 -0700

    make an interactive map showing the coverage data

--- _targets.R
+++ _targets.R
@@ -6,10 +6,14 @@ suppressPackageStartupMessages(library(dplyr))
 options(tidyverse.quiet = TRUE)
 tar_option_set(packages = c("cowplot",
                             "dataRetrieval",
+                            "htmlwidgets",
+                            "leaflet",
+                            "leafpop",
                             "lubridate",
                             "rnaturalearth",
                             "tidyverse",
-                            "urbnmapr"))
+                            "urbnmapr")
+               )
 
 # Load functions needed by targets below
 source("1_fetch/src/find_oldest_sites.R")
@@ -17,11 +21,12 @@ source("1_fetch/src/get_site_data.R")
 source("2_process/src/tally_site_obs.R")
 source("2_process/src/summarize_targets.R")
 source("3_visualize/src/map_sites.R")
+source("3_visualize/src/map_timeseries.R")
 source("3_visualize/src/plot_site_data.R")
 source("3_visualize/src/plot_data_coverage.R")
 
 # Configuration
-states <- c('WI', 'MN', 'MI', 'IL', 'IN')
+states <- c('WI', 'MN', 'MI', 'IL', 'IN', 'CA')
 parameter <- c('00060')
 
 # Targets
@@ -78,6 +83,15 @@ list(
   tar_target(
     site_map_png,
     map_sites("3_visualize/out/site_map.png", oldest_active_sites),
+    format = "file"
+  ),
+
+  # make an interactive map
+  tar_target(
+    interactive_state_summary_map_html,
+    map_timeseries(site_info = oldest_active_sites,
+                   plot_info_csv = summary_state_timeseries_csv,
+                   out_file = "3_visualize/out/timeseries_map.html"),
     format = "file"
   )
 )
