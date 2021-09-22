09402618817b50f654ae62c6e88c0344a613bc94
Jesse Ross <jross@usgs.gov>
2021-09-22 12:02:32 -0700

    add tallying and plotting steps

--- _targets.R
+++ _targets.R
@@ -16,6 +16,7 @@ source("1_fetch/src/find_oldest_sites.R")
 source("1_fetch/src/get_site_data.R")
 source("2_process/src/tally_site_obs.R")
 source("3_visualize/src/map_sites.R")
+source("3_visualize/src/plot_site_data.R")
 
 # Configuration
 states <- c('WI', 'MN', 'MI', 'IL', 'IN', 'IA')
@@ -32,12 +33,18 @@ list(
       mutate(state_plot_files = sprintf("3_visualize/out/timeseries_%s.png",
                                         state_abb)),
     tar_target(nwis_inventory,
-               filter(oldest_active_sites, state_cd == state_abb) ),
-    tar_target(nwis_data, get_site_data(site_info = nwis_inventory,
-                                        state = state_abb,
-                                        parameter = parameter)),
-    tar_target(site_tallies, tally_site_obs(nwis_data))
-    # Insert step for plotting data here
+      filter(oldest_active_sites, state_cd == state_abb)),
+    tar_target(nwis_data,
+      get_site_data(site_info = nwis_inventory,
+                    state = state_abb,
+                    parameter = parameter)),
+    tar_target(site_tallies,
+      tally_site_obs(nwis_data)),
+    tar_target(plot_sites,
+      plot_site_data(out_file = state_plot_files,
+                     site_data = nwis_data,
+                     parameter = parameter)),
+    names = state_abb
   ),
 
   # Map oldest sites
